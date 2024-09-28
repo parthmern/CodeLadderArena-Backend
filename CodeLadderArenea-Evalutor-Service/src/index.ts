da119453f1682fff2148617acf2f5bbec671900d
parthmern <parthmern@gmail.com>
2024-09-28 01:37:29 -0400

    Feat: bull board ui added

--- CodeLadderArenea-Evalutor-Service/src/index.ts
+++ CodeLadderArenea-Evalutor-Service/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express"; // Explicit type
 
+import serverAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -8,9 +9,11 @@ import SampleWorker from "./workers/SampleWorker";
 const app: Express = express();
 
 app.use("/api", apiRouter);
+app.use('/bullboardui', serverAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
   console.log(`server started running on- ${serverConfig.PORT}`);
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/bullboardui`);
 
   SampleWorker('SampleQueue');
 
--- CodeLadderArenea-Evalutor-Service/z_info.md
+++ CodeLadderArenea-Evalutor-Service/z_info.md
@@ -4,4 +4,6 @@
 
 ## HOW sample workers are working ?
 
+- redis docker run `docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest`
+
 ![imgg](https://res.cloudinary.com/dncm3mid4/image/upload/v1727460937/githubreadme/zpek14llftjkfs8ksayx.png)
