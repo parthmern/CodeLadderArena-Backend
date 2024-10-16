2496bb38c710c7f315a05140fc69102b0c0de9d8
parthmern <parthmern@gmail.com>
2024-10-15 21:29:56 -0400

    create docker container

---
+++ CodeLadderArenea-Evalutor-Service/src/containers/containerFactory.ts
@@ -0,0 +1,23 @@
+import Docker from 'dockerode';
+
+async function createContainer(imageName: string, cmdExecutable: string[]) {
+    
+    const docker = new Docker();
+
+    const container = await docker.createContainer({
+        Image: imageName,
+        Cmd: cmdExecutable,
+        AttachStdin: true, // to enable input streams
+        AttachStdout: true, // to enable output streams
+        AttachStderr: true, // to enable error streams
+        Tty: false, // teletypewriter and will run in non-interactive mode
+        HostConfig: {
+            Memory: 1024 * 1024 * 1024, // 2GB
+        },
+        OpenStdin: true // keep the input stream open even no interaction is there
+    });
+
+    return container;
+}
+
+export default createContainer;
