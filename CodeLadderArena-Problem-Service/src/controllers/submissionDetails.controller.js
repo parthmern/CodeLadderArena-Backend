7978173e20b07bf0004865f0f3c2ab758cf7a516
parthmern <parthmern@gmail.com>
2024-11-03 15:30:21 -0500

    bug : getSubmissionByUserAndProblem fixed

--- CodeLadderArena-Problem-Service/src/controllers/submissionDetails.controller.js
+++ CodeLadderArena-Problem-Service/src/controllers/submissionDetails.controller.js
@@ -1,5 +1,6 @@
 // Note: can make it as another service but just do with single file and here in this serivice
 
+const mongoose = require("mongoose");
 const { Submission } = require("../models");
 
 async function getSubmissionById(req, res) {
@@ -40,15 +41,20 @@ async function getSubmissionByUserAndProblem(req, res) {
     try {
         const { userId, problemId } = req.params;
         console.log("getSubmissionByUserAndProblem", userId, problemId);
-        const submissions = await Submission.find({ userId, problemId }).populate('problemId');
+        
+    
+        let submissions = await Submission.find({ userId });
 
-        if (submissions.length === 0) {
-            return res.status(404).json({ message: 'No submissions found for this user and problem' });
-        }
+        submissions = submissions.filter((submission)=>submission.problemId == problemId);
+
+        // if (submissions.length === 0) {
+        //     return res.status(404).json({ message: 'No submissions found for this user and problem' });
+        // }
 
         res.status(200).json(submissions);
     } catch (error) {
-        res.status(500).json({ message: 'Server error' });
+        console.log(error);
+        res.status(500).json({ message: 'Server error', error: error });
     }
 }
 
